b80a8843dc93b2e2e2bf638f72bb9dcf661ae3a3
Peter Bomars <Peter.Bomars@crowdstrike.com>
2015-02-18 15:06:47 -0800

    finish Ch7

---
+++ chap07/apache/metadata.rb
@@ -0,0 +1,8 @@
+name             'apache'
+maintainer       'Peter Bomars'
+maintainer_email 'pbomars@gmail.com'
+license          'MIT'
+description      'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version          '0.1.0'
+
